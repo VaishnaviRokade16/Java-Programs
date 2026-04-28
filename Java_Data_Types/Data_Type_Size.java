ed7a21a3c262675b7df8785af85348d2c2ca85fe
VaishnaviRokade16 <rokadevaishnavi520@gmail.com>
2026-04-29 00:35:40 +0530

    added Data Type Size Program

---
+++ Java_Data_Types/Data_Type_Size.java
@@ -0,0 +1,19 @@
+package Java_Data_Types;
+
+public class Data_Type_Size {
+
+    public static void main(String[] args){
+
+         System.out.println("Size of bytes: " + Byte.BYTES + "bytes");
+        System.out.println("Size of short: " + Short.BYTES + "bytes");
+        System.out.println("Size of int: " + Integer.BYTES + "bytes");
+        System.out.println("Size of long: " + Long.BYTES + "bytes");
+
+        System.out.println("Size of float: " + Float.BYTES + "bytes");
+        System.out.println("Size of double: " + Double.BYTES + "bytes");
+
+        System.out.println("Size of char: " + Character.BYTES + "bytes");
+        System.out.println("Size of boolean: Not fixed(JVM dependent)");
+    }
+    
+}

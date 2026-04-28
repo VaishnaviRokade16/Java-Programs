ab2b473f2ec8bd65abc026b9a286380f9e9e6a22
VaishnaviRokade16 <rokadevaishnavi520@gmail.com>
2026-04-29 00:29:55 +0530

    Added Primitive data type program

---
+++ Java_Data_Types/Primitive.java
@@ -0,0 +1,29 @@
+package Java_Data_Types;
+
+public class Primitive {
+    
+    public static void main(String[] args){
+        
+        byte b = 10;
+        short s = 100;
+        int i = 1000;
+        long l = 100000L;
+        float f = 10.5f;
+        double d = 99.9999;
+        char c = 'A';
+        boolean flag = true;
+
+         System.out.println("byte value: " +b);
+         System.out.println("Short value: "+s);
+         System.out.println("int value: "+i);
+         System.out.println("long value: "+l);
+         System.out.println("float value: "+f);
+         System.out.println("double value: "+d);
+         System.out.println("char value: "+c);
+         System.out.println("boolean value: "+flag);
+
+
+
+    }
+    
+}

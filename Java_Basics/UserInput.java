0d0437725ab37b5eab6680bcca1c386f155939ba
VaishnaviRokade16 <rokadevaishnavi520@gmail.com>
2026-04-29 00:16:56 +0530

    Added User Input Program

---
+++ Java_Basics/UserInput.java
@@ -0,0 +1,18 @@
+package Java_Basics;
+import java.util.Scanner;
+
+
+public class UserInput {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter you name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello " +name);
+        sc.close();
+
+    }
+    
+}

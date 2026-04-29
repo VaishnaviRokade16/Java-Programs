b6078d65d2e689247c3cd0ea0194d68b0e520b3f
VaishnaviRokade16 <rokadevaishnavi520@gmail.com>
2026-04-29 16:26:56 +0530

    Added a If Loop Example Program

---
+++ Java_Loops/IfLoopExample.java
@@ -0,0 +1,21 @@
+package Java_Loops;
+import java.util.Scanner;
+
+public class IfLoopExample {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a Number: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+        if(i % 2 == 0){
+            System.out.println(i);
+
+            }
+        }
+            sc.close();
+    }
+    
+}

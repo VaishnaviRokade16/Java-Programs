e9dab77cc7ac1a11f6bda64c4459d47d2b2d0652
VaishnaviRokade16 <rokadevaishnavi520@gmail.com>
2026-04-29 15:32:01 +0530

    Added Count Digits Program

---
+++ Java_Loops/CountDigit.java
@@ -0,0 +1,28 @@
+package Java_Loops;
+import java.util.Scanner;
+
+public class CountDigit {
+
+    public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a Number: ");
+    int num = sc.nextInt();
+
+    int count = 0;
+
+    while ( num != 0){
+        count++;
+
+        num = num / 10;
+
+    }
+
+    System.out.println("Number of Digits = " + count);
+
+    sc.close();
+
+}
+    
+}

22e4e8e98f4ce55cfc726efbcf5e2d9eecf11ebc
VaishnaviRokade16 <rokadevaishnavi520@gmail.com>
2026-04-29 16:33:38 +0530

    Added a Factorial Program

---
+++ Java_Loops/Factorial.java
@@ -0,0 +1,25 @@
+package Java_Loops;
+import java.util.Scanner;
+
+
+public class Factorial {
+
+    public static void main(String[] args){
+
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.println(" Enter a number: " );
+        int n = sc.nextInt();
+
+        int fact = 1;
+
+        for(int i = 1; i <= n; i++ ){
+            fact = fact*1;
+
+        }
+
+        System.out.println("Factorial = " + fact);
+        sc.close();
+    }
+    
+}
